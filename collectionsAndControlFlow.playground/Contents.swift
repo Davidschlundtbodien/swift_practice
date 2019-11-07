ada3d1b65f0790d0a1cb8b14012c663d9f8595f1
davidschlundtbodien <d.schlundtbodien@gmail.com>
2019-11-07 12:02:08 -0800

    Adds intro to collections/arrays

---
+++ collectionsAndControlFlow.playground/Contents.swift
@@ -0,0 +1,36 @@
+//Intro to collections
+
+var todo: [String] = ["Vacuum", "Make lunch", "Make Git repo for swift"]
+
+// adding items to an array
+
+todo.append("Feed Dogs")
+
+todo + ["Laundry"] // does not mutate list
+
+todo += ["Bake Bread"] // mutates list
+
+// Immutable array
+let secondTaskList: [String] = ["Only task allowed"]
+
+//secondTaskList.append(contentsOf: "throws an error")
+//secondTaskList += ["Also throws an error"]
+
+//Reading from Arrays
+
+todo[0] // subscripting
+
+// mutating an array
+
+todo[3] = "Clean sink"
+
+// Insert using indexes
+
+todo.insert("Watch TV", at: 3)
+
+// Removing items from array
+todo.count
+
+todo.remove(at: 1)
+
+todo.count
